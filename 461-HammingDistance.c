5d46f4397ab6e6996d3f67b8613a87e95e1b53d6
ryanfanchiotti <ryan@rur.com>
2024-04-19 00:52:53 -0400

    comments

--- 461-HammingDistance.c
+++ 461-HammingDistance.c
@@ -8,8 +8,8 @@ int hammingDistance(int x, int y) {
 __attribute__((naked))
 int hammingDistance(int x, int y) {
     __asm__(
-        "xor %edi, %esi;"
-        "popcnt %esi, %eax;"
+        "xor %edi, %esi;" // xor x and y and store in y
+        "popcnt %esi, %eax;" // put population count of 1's in return register
         "ret;"
     );
 }
