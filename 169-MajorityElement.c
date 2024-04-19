f106bff6e55869208814ad162b6b7cf297b9a080
ryanfanchiotti <112440089+ryanfanchiotti@users.noreply.github.com>
2024-04-18 20:45:14 -0400

    Add files via upload
    added files

---
+++ 169-MajorityElement.c
@@ -0,0 +1,55 @@
+/* C solution using Boyer-Moore majority vote algorithm:
+int majorityElement(int* nums, int numsSize) {
+    int votes = 1, cur = *nums;
+    for (int i = 1; i < numsSize; i++){
+        if (votes == 0){
+            cur = nums[i];
+            votes++;
+        } else if (cur == nums[i]) votes++;
+        else votes--;
+    }
+    return cur;
+} */
+
+// at&t x86_64 solution using stack frame:
+__attribute__((naked)) 
+int majorityElement(int* nums, int numsSize) {
+    __asm__(
+    "pushq %rbp;" // push frame pointer onto stack
+    "sub $16, %rsp;" // creating stack space for votes, index, and cur
+    "movq $1, -8(%rbp);" // init votes to 1
+    "movq $1, -16(%rbp);" // init index to 1
+    "movl (%rdi), %eax;" // init cur to nums[0], storing it in return register
+
+    "loop:;" 
+    "cmpl %esi, -16(%rbp);" // while index is less than size
+    "jge end;"
+    "mov -16(%rbp), %rcx;" // move index to rcx and index into nums
+    "mov (%rdi, %rcx, 4), %edx;" // move nums[i] to edx
+    
+    "votescheck:;" // check if votes is 0
+    "cmpq $0, -8(%rbp);"
+    "jne curcheck;"
+    "movq $1, -8(%rbp);" // set votes to 1 and move nums[i] to cur
+    "movl %edx, %eax;"
+    "jmp endloop;"
+    
+    "curcheck:;" // check if cur is equal to nums[i]
+    "cmpl %eax, %edx;" // only need to check low end of regs since int
+    "jne decrementvotes;"
+    "addq $1, -8(%rbp);" // increment votes for cur
+    "jmp endloop;"
+
+    "decrementvotes:;"
+    "subq $1, -8(%rbp);" // decrement votes for cur
+
+    "endloop:;" // increment index and go to start of loop
+    "addq $1, -16(%rbp);"
+    "jmp loop;"
+
+    "end:;"
+    "add $16, %rsp;" // clean up stack and return
+    "popq %rbp;"
+    "retq;" 
+    );
+}
---
+++ 55-JumpGame.c
@@ -0,0 +1,43 @@
+/* C solution using concept of maximum gas from previous element
+int canJump(int* nums, int numsSize) {
+    int gas = 0;
+    for (int i = 0; i < numsSize - 1; i++){
+        gas--;
+        if (gas < nums[i]) gas = nums[i];
+        if (gas < 1) return 0;
+    }
+    return 1;
+} */
+
+// at&t x86_64 solution using 32 bit registers as variables instead of stack frame
+__attribute__((naked))
+int canJump(int* nums, int numsSize){
+    __asm__(
+        "movl $1, %eax;" // move 1 into return register
+        "xor %rdx, %rdx;" // init index to 0
+        "xor %ecx, %ecx;" // init gas to 0
+        "dec %esi;" // moving from last index will not be checked
+
+        "loop:;"
+        "cmp %edx, %esi;" // check if index is size - 1
+        "jle end;"
+
+        "dec %ecx;" // decrement gas after moving indices
+        "cmpl (%rdi, %rdx, 4), %ecx;" // check if nums[i] has more gas
+        "jge gascheck;"
+        "movl (%rdi, %rdx, 4), %ecx;"
+
+        "gascheck:;" // return false if no more gas
+        "cmpl $1, %ecx;"
+        "jge endloop;"
+        "xor %eax, %eax;"
+        "jmp end;"
+
+        "endloop:;" // increment index and go back to start of loop
+        "inc %edx;"
+        "jmp loop;"
+
+        "end:;"
+        "ret;"
+    );
+}
